a2dc5bf1a051b79c784dfa1bfdbc787ce6f74be6
Tim Wiese <tim.wiese@esa.int>
2020-09-25 19:12:29 +0200

    Check lsmod to see if pcan is properly installed in order to reinstall in case of kernel upgrade

--- init.rb
+++ init.rb
@@ -1,6 +1,4 @@
 def has_pcan_api?
-    include_file = File.join("/usr","include","pcan.h")
-    lib = File.join("/usr","lib","libpcan.so")
-    File.exists?(include_file) && File.exists?(lib)
+    system("lsmod | grep pcan > /dev/null")
 end
 
