be17bdfc325f8619b02389aa6575df40172af766
Tim Wiese <tim.wiese@esa.int>
2020-09-27 15:07:25 +0200

    Don't do pcan kernel module check in docker

--- init.rb
+++ init.rb
@@ -3,3 +3,7 @@ def has_pcan_api?
     system("lsmod | grep pcan > /dev/null")
 end
 
+# checks RUNNING_IN_DOCKER environment variable
+def running_in_docker?
+    ENV.has_key?('RUNNING_IN_DOCKER') and ENV['RUNNING_IN_DOCKER'] == 'true'
+end
